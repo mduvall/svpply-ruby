12fb55ba3ace92328025d00f5b268182a20ba4bb
Matthew DuVall <mduvall89@gmail.com>
2013-04-17 22:10:22 -0700

    Similar products endpoint

--- lib/svpply/products.rb
+++ lib/svpply/products.rb
@@ -3,5 +3,10 @@ module Svpply
     def product(id)
       get('products/' + id.to_s + '.json', {})
     end
+
+    # Products similar to a product with <id> id
+    def similar_products(id)
+      get('products/' + id.to_s + '/similars.json', {})
+    end
   end
 end
